9358ab2932e4c6e091d56fe00402d4f29a7d440c
BOX <thomas.box@sfr.fr>
2020-03-04 11:31:05 +0100

    test getX et setX ok

--- src/classes/main.cpp
+++ src/classes/main.cpp
@@ -17,22 +17,25 @@ int main()
     // initialisation des pointeurs
     fenetre = Board::getInstance ();
 
-    Point p(10,1);
+    Point p(10,4);
     p.drawPoint();
-    Point p2(10,9);
+    Point p2(10,5);
     p2.drawPoint();
     p.moveUp();
     p.drawPoint();
     p.moveDown();
     p.drawPoint();
-    p.moveLeft();
-    p.drawPoint();
-    p.moveRight();
-    p.drawPoint();
+    cout << "La valeur est de: " << p.getX();
+    p.setX(5);
+    cout << "La valeur est de: " << p.getX();
     
-    p.erasePoint();
-    
-    p.debug();
+    p.drawPoint();
+//    p.moveLeft();
+//    p.drawPoint();
+//    p.moveRight();
+//    p.drawPoint();
+//    p.erasePoint();
+//    p.debug();
     
     //cout << "press any key to quit" << endl;
     getchar();
