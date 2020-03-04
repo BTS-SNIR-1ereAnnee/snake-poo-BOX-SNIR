f9eb4e8508ae179d349b0cb3c4da0d2b7d1f5392
BOX <thomas.box@sfr.fr>
2020-03-04 12:29:04 +0100

    ossature Snake

---
+++ src/classes/Snake.cpp
@@ -0,0 +1,8 @@
+
+#include "Snake.h"
+
+using namespace std;
+
+
+
+
---
+++ src/classes/Snake.h
@@ -0,0 +1,14 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+class Snake
+{
+public:
+    
+protected:
+
+private:
+   
+};
+
+#endif // POINT_H
